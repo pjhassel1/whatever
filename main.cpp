4aa5458892ddff61b0bb6497932e6ac2d7d23758
Jeff Hasselbring <hassej@pcps.us>
2016-03-07 12:47:22 -0600

    3/7/2016 changes

--- main.cpp
+++ main.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
+#include "include/map.h"
+#include "include/items.h"
+#include "include/room.h"
+
+
 
 int main()
 {
+	Map map;
+
+
 	std::cout << "whatever" << std::endl;
 
 	/* Only run this in windows */
---
+++ source/setup_map.cpp
@@ -0,0 +1,10 @@
+#include "setup_map.h"
+// rm #, rm name, rm description
+bool setup_map(Map &map)
+{
+	map.add_room(1, "Teleport Room", 
+		""):
+	map.add_room(2)
+	return true;
+
+}
